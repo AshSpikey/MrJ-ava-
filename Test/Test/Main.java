1f3d4c97b2b8cfb062e9a29f3a0ae9af216c6031
AshSpikey <aspeicher30@ladueschools.net>
2022-10-13 13:29:34 +0000

    commit rq

--- Test/Test/Main.java
+++ Test/Test/Main.java
@@ -1,7 +1,11 @@
-
+package Test;
 public class Main{
     public static void main(String[] args){
         System.out.println("\033[H\033[2J"); //This clears the terminal because I think the massive amount of random characters are stupid
-        System.out.println("Sayam is pretty cool");
+        //System.out.println("Sayam is pretty cool");
+
+
+        Test testVar = new Test();
+        System.out.println(testVar.getFirstName());
     }
 }
---
+++ Test/Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+public class Test {
+    String firstName;
+    public Test(){
+        
+    }
+    
+    public String getFirstName(){
+        firstName = ("Hello Hello Hello!");
+        return firstName;
+    }
+}
