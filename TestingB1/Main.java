31361be00af9bbda9a864cccff9af8140bfbffa9
AshSpikey <aspeicher30@ladueschools.net>
2022-10-13 13:10:02 +0000

    Initial test commit

---
+++ TestingB1/Main.java
@@ -0,0 +1,7 @@
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\033[H\033[2J"); //This clears the terminal because I think the massive amount of random characters are stupid
+        System.out.println("Sayam is pretty cool");
+    }
+}
